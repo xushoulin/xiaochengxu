f70c3d554eed88a5693ab36578c3b18e865f7ee3
20180402PC\Administrator <xushoulin@flyfnd.com>
2018-05-25 13:32:58 +0800

    fir

---
+++ pages/index/index.js
@@ -0,0 +1,34 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+    list:[1,2,3,4]
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    this.setData({
+
+    })
+  },
+  toDetail:function(e){
+    console.log(e)
+    let index = e.currentTarget.dataset.index;
+   console.log(index)
+  }
+})
